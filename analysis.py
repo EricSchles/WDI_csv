9cb3031a1ff5da4f7b26a9eadb9f2ffdc1a07816
EricSchles <ericschles@gmail.com>
2015-01-12 21:27:45 -0500

    first commit

---
+++ analysis.py
@@ -0,0 +1,12 @@
+import statsmodels
+import pandas
+import matplotlib
+
+# df3 = DataFrame(randn(1000, 2), columns=['B', 'C']).cumsum()
+
+# df3['A'] = Series(list(range(len(df))))
+
+# df3.plot(x='A', y='B')
+
+# import matplotlib.pyplot as plt
+# plt.figure(); df.plot();
